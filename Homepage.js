c95dc41ce4b32bbc903a8826989ad7dffaac0dea
bradkerwin <bkerwin101@gmail.com>
2024-08-12 18:07:28 -0400

    carousel created and various functions fixed

--- Homepage.js
+++
@@ -1,9 +0,0 @@
-function changePikachu() {
-    const newImagesrc = "new_fighting_pikachu.png";
-    document.getElementById("pikachu-image").src = newImagesrc;
-}
-
-function revertPikachu() {
-    const newImagesrc = "og_pikachu.jpg";
-    document.getElementById("pikachu-image").src = newImagesrc;
-}
--- PokemonSearchPage.js
+++ PokemonSearchPage.js
@@ -1,9 +1,16 @@
 console.log("Testing");
 async function fetchPokemonData(pokemonName) {
+  try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`); // Obtaining the info from the website.
     const pokemonData = await response.json(); // Converts the fetched data into an object.
     console.log(pokemonData);
     return pokemonData;
+  }
+  catch(error){
+    console.log("Error fetching data:", error);
+    const userInfoElement = document.getElementById("pokemon-info");
+    userInfoElement.textContent = "Failed to fetch pokemon data";
+  }
 }
 
 const handleSubmit = async (event)=>{
@@ -13,30 +20,24 @@ const handleSubmit = async (event)=>{
     const pokemonData = await fetchPokemonData(pokemonName);
     const pokemonInfoElement = document.getElementById("pokemon-info");
 
-  
-    pokemonInfoElement.innerHTML = `<h2>${pokemonData.name}</h2>
-    <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
-    <h3>Abilities</h3>
-    <ul>
-        ${pokemonData.abilities.map(a => `<li>${a.ability.name}</li>`).join("")}
-    </ul>
-    <h3>Base Experience</h3>
-    <p>${pokemonData.base_experience}</p>
-    <h3>Sounds:</h3>
-    <audio src="${pokemonData.cries.latest}" controls></audio><br>
-    <audio src="${pokemonData.cries.legacy}" controls></audio>`
-}
+    if (pokemonData) {
 
-async function displayUserData() {
-    try{
-      const userData = await fetchPokemonData();
-      const userInfoElement = document.getElementById("pokemon-info");
-    }
-    catch(error){
-      console.log("Error fetching data:", error);
-      const userInfoElement = document.getElementById("pokemon-info");
-      userInfoElement.textContent = "Failed to fetch pokemon data";
-    }
+      pokemonInfoElement.innerHTML = `
+      <div class="card">
+        <h2 class="card-title text-center">${pokemonData.name}</h2>
+        <div class="card-body text-center">
+          <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+          <h3>Abilities</h3>
+          <ul>
+              ${pokemonData.abilities.map(a => `<li>${a.ability.name}</li>`).join("")}
+          </ul>
+          <h3>Base Experience</h3>
+          <p>${pokemonData.base_experience}</p>
+          <h3>Sounds:</h3>
+          <audio src="${pokemonData.cries.latest}" controls></audio><br>
+          <audio src="${pokemonData.cries.legacy}" controls></audio>
+        </div>
+      </div>
+      `
   }
-
-
+}
